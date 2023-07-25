394efbd80170361d48236e19f1ed20d084a3215d
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2023-07-25 18:12:37 +0500

    order button

--- src/App.jsx
+++ src/App.jsx
@@ -53,22 +53,23 @@ function App() {
   };
 
   const onSendData = useCallback(() => {
-    const queryID = telegram.initDataUnsafe?.query_id;
+      telegram.sendData(JSON.stringify(cartItems))
+    // const queryID = telegram.initDataUnsafe?.query_id;
 
-    if (queryID) {
-      fetch("https://telegramwebapibot-b671371abfbb.herokuapp.com/web-data", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          products: cartItems,
-          queryID: queryID,
-        }),
-      });
-    } else {
-      telegram.sendData(JSON.stringify(cartItems));
-    }
+    // if (queryID) {
+    //   fetch("https://telegramwebapibot-b671371abfbb.herokuapp.com/web-data", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       products: cartItems,
+    //       queryID: queryID,
+    //     }),
+    //   });
+    // } else {
+    //   telegram.sendData(JSON.stringify(cartItems));
+    // }
   }, [cartItems]);
 
   useEffect(() => {

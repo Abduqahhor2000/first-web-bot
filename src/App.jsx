a70b29fc0bd3a9e9220ef4700ebefc0ce130b9bc
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2023-07-25 18:03:51 +0500

    salomlarrr

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Card from "./components/card/card";
 import Cart from "./components/cart/cart";
 import { getData } from "./constants/db";
 
-const telegram = window.Telegram.WebApp
+const telegram = window.Telegram.WebApp;
 
 const courses = getData();
 
@@ -12,58 +12,83 @@ function App() {
   const [cartItems, setCartItems] = useState([]);
   console.log(cartItems);
 
-  useEffect(()=>{
-    telegram.ready()
-  })
+  useEffect(() => {
+    telegram.ready();
+  });
 
-  const onAddItem = item => {
-		const existItem = cartItems.find(c => c.id == item.id);
+  const onAddItem = (item) => {
+    const existItem = cartItems.find((c) => c.id == item.id);
 
-		if (existItem) {
-			const newData = cartItems.map(c =>
-				c.id == item.id
-					? { ...existItem, quantity: existItem.quantity + 1 }
-					: c
-			);
-			setCartItems(newData);
-		} else {
-			const newData = [...cartItems, { ...item, quantity: 1 }];
-			setCartItems(newData);
-		}
-	};
+    if (existItem) {
+      const newData = cartItems.map((c) =>
+        c.id == item.id ? { ...existItem, quantity: existItem.quantity + 1 } : c
+      );
+      setCartItems(newData);
+    } else {
+      const newData = [...cartItems, { ...item, quantity: 1 }];
+      setCartItems(newData);
+    }
+  };
 
-  const onRemoveItem = item => {
-		const existItem = cartItems.find(c => c.id == item.id);
+  const onRemoveItem = (item) => {
+    const existItem = cartItems.find((c) => c.id == item.id);
 
-		if (existItem.quantity === 1) {
-			const newData = cartItems.filter(c => c.id !== existItem.id);
-			setCartItems(newData);
-		} else {
-			const newData = cartItems.map(c =>
-				c.id === existItem.id
-					? { ...existItem, quantity: existItem.quantity - 1 }
-					: c
-			);
-			setCartItems(newData);
-		}
-	};
+    if (existItem.quantity === 1) {
+      const newData = cartItems.filter((c) => c.id !== existItem.id);
+      setCartItems(newData);
+    } else {
+      const newData = cartItems.map((c) =>
+        c.id === existItem.id
+          ? { ...existItem, quantity: existItem.quantity - 1 }
+          : c
+      );
+      setCartItems(newData);
+    }
+  };
 
   const onCheckout = () => {
-		telegram.MainButton.text = 'Sotib olish :)';
-		telegram.MainButton.show();
+    telegram.MainButton.text = "Sotib olish :)";
+    telegram.MainButton.show();
     console.log("hahahahahah");
-	};
+  };
+
+  const onSendData = useCallback(() => {
+    const queryID = telegram.initDataUnsafe?.query_id;
+
+    if (queryID) {
+      fetch("https://telegramwebapibot-b671371abfbb.herokuapp.com/web-data", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          products: cartItems,
+          queryID: queryID,
+        }),
+      });
+    } else {
+      telegram.sendData(JSON.stringify(cartItems));
+    }
+  }, [cartItems]);
+
+  useEffect(() => {
+    telegram.onEvent("mainButtonClicked", onSendData);
+
+    return () => telegram.offEvent("mainButtonClicked", onSendData);
+  }, [onSendData]);
 
   return (
     <>
       <h1>Dasturlash kurslar</h1>
-      <Cart 
-        cartItems={cartItems} 
-        onCheckout={onCheckout} 
-      />
+      <Cart cartItems={cartItems} onCheckout={onCheckout} />
       <div className="cards__container">
         {courses.map((course) => (
-          <Card key={course.id} course={course} onAddItem={onAddItem} onRemoveItem={onRemoveItem} />
+          <Card
+            key={course.id}
+            course={course}
+            onAddItem={onAddItem}
+            onRemoveItem={onRemoveItem}
+          />
         ))}
       </div>
     </>
